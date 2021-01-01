02da0167d44546d85a3f0c5f435cda7c831f45ed
Jody <jgilpin0713@gmail.com>
2021-01-01 17:29:04 -0500

    Fixed imports

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
+from forex_python import converter
 from forex_python.converter import CurrencyCodes
 
 #from forex_python.converter import CurrencyRates
